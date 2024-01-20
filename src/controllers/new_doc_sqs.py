844e17eda872e5dd146513da7202b1cfb78b125f
rachhtord <rachael.tordoff@hotmail.co.uk>
2024-01-20 12:34:09 +0000

    added new html templte

--- src/controllers/new_doc_sqs.py
+++ src/controllers/new_doc_sqs.py
@@ -1,7 +1,7 @@
+from email.message import Message
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
+from flask import render_template
+from src.dependencies.smtp_config import send_mail
 
 from src import config
 
@@ -9,33 +9,24 @@ from src import config
 class NewDocSqs(object):
     def __init__(self, body):
         print("doing some stuff", flush=True)
-        send_email(body['email'], body['formatted address'], body['report_id'])
+        send_email_initial(body)
+
         print("ok finished now", flush=True)
 
 
-def send_email(recipient, formatted_address, report_id):
+def send_email_initial(body):
+
+
     base_domain = config.BASE_DOMAIN
     # Define these once; use them twice!
-    strFrom = 'noreply@ukpropertyreport.co.uk'
-    strTo = recipient
+    strFrom = body['from_email']
+    strTo = body['email']
 
-    # Create the root message and fill in the from, to, and subject headers
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = f'Your property report for {formatted_address}'
-    msgRoot['From'] = strFrom
-    msgRoot['To'] = strTo
-    msgRoot.preamble = 'This is a multi-part message in MIME format.'
+    html_content = render_template('../tempates/pages/enquiry_email.html', body=body)
+    return send_mail(
+        body['email_title'],
+        strFrom,
+        [strTo],
+        html_content
+        )
 
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
-
-    # We reference the image in the IMG SRC attribute by the ID we give it below
-    msgText = MIMEText(
-        f'<b>Here is your property report</b><br><br><a href="{base_domain}/report/{report_id}" style="background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;">Download Report</a>', 'html')
-    msgAlternative.attach(msgText)
-
-    # Send the email
-    smtp = smtplib.SMTP('217.160.185.253', 2500)
-    # smtp.login(strFrom, '')
-    smtp.sendmail(strFrom, strTo, msgRoot.as_string())
-    smtp.quit()
---
+++ src/dependencies/smtp_config.py
@@ -0,0 +1,57 @@
+# this code must use environment variables for original config as we will be changing the application config
+
+import os
+from src import app
+from flask_mail import Mail
+from flask import current_app
+from flask_mail import Message
+from src.exceptions import ApplicationError
+# from src.sql import Sql
+
+def get_smtp_config(email):
+	mail = Mail()
+	# results = Sql.get_email_account_for_company(company_name)
+	# print(results)
+	# if len(results) == 1:
+	# 	email_account = results[0]
+	# 	app.config.update(dict(
+	# 		MAIL_SERVER = email_account.server,
+	# 		MAIL_PORT = email_account.port,
+	# 		MAIL_USE_TLS = email_account.tls,
+	# 		MAIL_USE_SSL = email_account.ssl,
+	# 		MAIL_USERNAME = email_account.username,
+	# 		MAIL_PASSWORD = email_account.password,
+	# 	))
+	# else:
+	app.config.update(dict(
+		MAIL_SERVER = os.environ['MAIL_SERVER'],
+		MAIL_PORT = os.environ['MAIL_PORT'],
+		MAIL_USE_TLS = False,
+		MAIL_USE_SSL = False,
+		MAIL_USERNAME = email
+		# MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+	))
+
+
+	mail.init_app(app)
+	return mail
+
+def send_mail(title, recipient, body, html_content):
+	mail = get_smtp_config('')
+	msg = Message(
+            title,
+            sender=app.config['MAIL_USERNAME'],
+            recipients=recipient
+            )
+
+	msg.html = html_content
+
+	try:
+		mail.send(msg)
+	except Exception as e:
+		print(e)
+		current_app.logger.error('Unhandled Exception: %s', repr(e))
+		raise ApplicationError('something has gone sending email', 'unspecified')
+	else:
+		current_app.logger.info('email sent')
+		return "sent"
